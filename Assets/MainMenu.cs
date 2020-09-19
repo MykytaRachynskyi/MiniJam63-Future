2cef3c149a3d2eaccb61c15e180df242d243761f
bqd88 <skruff3@gmail.com>
2020-09-19 14:39:16 +0300

    Add something

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] bool buttonTrue;
+    //[SerializeField] GameEvent exitManagement;
+    //public Animator transition;
+
+    public float transitionTime = 1f;
+
+    public void PlayGame() {
+        if (buttonTrue == true) {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+    }
+
+    public void doExitGame() {
+            Application.Quit();
+    }
+
+    void Awake() {
+        if (buttonTrue != true) {
+            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        }
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {
+        if (buttonTrue != true) {
+            yield return new WaitForSeconds(transitionTime);
+        }
+        //transition.SetTrigger("Start");
+        if (buttonTrue == true) {
+            yield return new WaitForSeconds(transitionTime);
+            //exitManagement.Invoke();
+            //Destroy(gameObject);
+        }
+        SceneManager.LoadScene(levelIndex);
+    }
+}
---
+++ Assets/PressAction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressAction : MonoBehaviour
+{
+    public int TileVar;
+    public void InitTile(int ID) {
+        TileVar = ID;
+    }
+}
